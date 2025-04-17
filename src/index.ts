1c8227e402ac35d8458f5f48c2838d789ef0e560
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2025-04-17 22:18:41 +0900

    add: dynamoDBClient

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { Mora, MoraNode, tokenize } from "manimani";
-import {
-	startServerAndCreateLambdaHandler,
-	handlers,
-} from '@as-integrations/aws-lambda';
+import { startServerAndCreateLambdaHandler, handlers } from '@as-integrations/aws-lambda';
+import { DynamoDBClient, QueryCommand, QueryCommandInput } from "@aws-sdk/client-dynamodb";
 
 type Status = "correct" | "incorrect" | "unanswered";
 
@@ -17,8 +15,43 @@ type MoraNodeWithStatus =  MoraNode & {
     status: Status;
 }
 
+const client = new DynamoDBClient({region: "ap-noatheast-1"});
 const getTypingThemeResolver = async(args: {id: number, level: number, difficulty: number}) => {
-    return { id: 1, text: "", ruby: "row.ruby", moras: {} };
+    const group = Math.round(args.difficulty * 10) / 10;
+    const params: QueryCommandInput = {
+        TableName: "sentence",
+        KeyConditionExpression: "#level = :level AND difficult_group = :difficult_group",
+        FilterExpression: "#id <> :id",
+        ExpressionAttributeNames: {
+            "#level": "level",
+            "#difficult_group": "difficult_group",
+            "#id": "id"
+        },
+        ExpressionAttributeValues: {
+            ":level": { S: args.level.toString() },
+            ":difficult_group": { S: group.toString() },
+            ":id": { S: args.id.toString() }
+          },
+        Limit: 100,
+        ScanIndexForward: true
+    }
+
+    try {
+        const data = await client.send(new QueryCommand(params));
+
+        // TODO difficultに近い値を一件取得
+        // TODO morawithstatusに変換
+        // TODO レスポンスの生成
+        // TODO テーブルの作成と初期データの投入方法を考える
+
+        return { id: 1, text: "", ruby: "row.ruby", moras: {} };
+    } catch(err) {
+        // TODO レスポンスの生成
+        return {
+            status: 500
+        }
+    }
+
 }
 
 const toTokens = async(sentence: { text: string, ruby: string }): Promise<Mora[]> => {
@@ -58,26 +91,27 @@ const resolvers = {
 };
 
 const typeDefs = `#graphql
-type TypingTheme {
-    id: Int!
-    text: String!
-    ruby: String!
-    moras: String!
-}
-
-type Query {
-    getTypingTheme(id: Int, level: Int!, difficulty: Float!): TypingTheme
-}
-
-query getTypingTheme($id: Int, $level: Int!, $difficulty: Float!) {
-    getTypingTheme(id: $id, level: $level, difficulty: $difficulty) {
-        id
-        text
-        ruby
-        moras
+    type TypingTheme {
+        id: Int!
+        text: String!
+        ruby: String!
+        moras: String!
     }
-}
+
+    type Query {
+        getTypingTheme(id: Int, level: Int!, difficulty: Float!): TypingTheme
+    }
+
+    query getTypingTheme($id: Int, $level: Int!, $difficulty: Float!) {
+        getTypingTheme(id: $id, level: $level, difficulty: $difficulty) {
+            id
+            text
+            ruby
+            moras
+        }
+    }
 `;
+
 const server = new ApolloServer({
 	typeDefs,
 	resolvers
@@ -86,4 +120,4 @@ const server = new ApolloServer({
 export const graphqlHandler = startServerAndCreateLambdaHandler(
 	server,
 	handlers.createAPIGatewayProxyEventV2RequestHandler()
-  );
+);
